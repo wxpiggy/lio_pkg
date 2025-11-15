2720df9338174963a19f22f41dcd9ec2a7f2f7f8
root <1014714182@qq.com>
2025-11-15 13:00:02 +0800

    remove preinteg and iekf

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -34,18 +34,18 @@ include_directories(${PCL_INCLUDE_DIRS})
 # include_directories(${OpenCV_INCLUDE_DIRS})
 
 # g2o 使用thirdparty中的
-include_directories(/thirdparty/g2o/)
-set(g2o_libs
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_stuff.so
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_core.so
-	${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_cholmod.so
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_dense.so
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_csparse.so
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_csparse_extension.so
-        ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_types_sba.so
-        ${CSPARSE_LIBRARY}
-        ${CHOLMOD_LIBRARY}
-        )
+# include_directories(/thirdparty/g2o/)
+# set(g2o_libs
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_stuff.so
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_core.so
+# 	${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_cholmod.so
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_dense.so
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_csparse.so
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_csparse_extension.so
+#         ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_types_sba.so
+#         ${CSPARSE_LIBRARY}
+#         ${CHOLMOD_LIBRARY}
+#         )
 find_package(catkin REQUIRED COMPONENTS
         roscpp
         rospy
@@ -73,7 +73,7 @@ include_directories(${PROJECT_SOURCE_DIR}/thirdparty/robin-map/include)
 
     set(third_party_libs
             ${catkin_LIBRARIES}
-            ${g2o_libs}
+        #     ${g2o_libs}
         #     ${OpenCV_LIBS}
             ${PCL_LIBRARIES}
         #     ${Pangolin_LIBRARIES}
